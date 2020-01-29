df6d7cb4d4a086a5e23cb232e1d1b51179171306
Ango <ango.gomes@gmail.com>
2020-01-29 22:13:18 +0100

    Added Database Class, first version

--- app.py
+++ app.py
@@ -9,16 +9,22 @@
 
 import requests
 from os import environ
-from openfoodfact.api import Api
+from openfoodfacts.api import Api
+from database.database import Database
+from typing import BinaryIO
 
 class App:
     ''' Main Class '''
 
     def __init__(self) -> None:
         self.api: Api = Api()
+        self.database : Database() = Database(environ['HOST'], environ['USER'], environ['PASSWORD'])
 
     def main(self) -> None:   
         self.api.get_products()
+        self.database.connect()
+        print(self.database.execute("SHOW DATABASES ;"))
+        self.database.disconnect()
 
 def main() -> None:
     app: App = App()
---
+++ database/database.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+'''
+@desc    description
+@author  ANGO <ango@afnor.org>
+@version 0.0.1
+@date    2020-01-29
+@note    0.0.1 (2020-01-29) : Init file
+'''
+
+import mysql.connector
+
+class Database:
+
+    def __init__(self, host, user, passwd, db=None, charset="utf8"):
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.db = db
+        self.charset = charset
+        self.connection = None
+
+    def connect(self):
+        '''Database connection.'''
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            db=self.db,
+            user=self.user,
+            passwd=self.passwd,
+            charset=self.charset
+        )
+
+    def execute(self, query):
+        '''Open Cursor, Execute the Query and return result.'''
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+
+    def disconnect(self):
+        '''Close the database connection.'''
+        self.connection.close()
+    
+    def __del__(self):
+        if self.connection is not None:
+            self.disconnect()
